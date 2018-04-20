77ab726f611d7b2c22cf68ae5c28c032a0bc3900
onchari wycliffe <onchariwycliffe@gmail.com>
2018-04-20 07:30:56 +0300

    First commit

---
+++ ExporeCalifornia/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+namespace ExporeCalifornia.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
